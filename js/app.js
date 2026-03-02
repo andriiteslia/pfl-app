6012227ff5b14fe17840d1aae0a70c200f636bfd
andriiteslia <teslyaandriy@gmail.com>
2026-03-03 01:53:50 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -157,7 +157,7 @@ function initTelegram() {
     const tg = window.Telegram?.WebApp;
     if (!tg) return;
     
-    const FULLSCREEN_PAD = 120;
+    const FULLSCREEN_PAD = 108;
     const FULLSIZE_PAD = 20;
     
     function setPad(px) {
