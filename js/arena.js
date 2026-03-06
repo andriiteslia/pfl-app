bbc3929d636e4624264a267014529da37313a639
andriiteslia <teslyaandriy@gmail.com>
2026-03-06 17:00:55 +0200

    Update arena.js

--- js/arena.js
+++ js/arena.js
@@ -5,7 +5,7 @@
 
 import CONFIG from './config.js';
 import { fetchSheetData } from './api.js';
-import { $, $$, escapeHtml, setButtonLoading, haptic, parseDividers, shareCard, buildShareLink, SHARE_ICON_SVG } from './utils.js';
+import { $, $$, escapeHtml, setButtonLoading, haptic, parseDividers, shareCard, buildShareLink, SHARE_ICON_SVG, showToast } from './utils.js';
 
 // ---- State ----
 let tags = [];
@@ -494,6 +494,7 @@ function renderArenaContent() {
   setArenaState('content');
   loaded = true;
   dataReady = false;
+  showToast('Оновлено ✓');
 }
 
 // ---- Render deferred content when tab becomes active ----
